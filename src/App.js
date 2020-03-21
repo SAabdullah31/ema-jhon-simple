7d05eeba2af4249cd46f79a5c106e565fd9d2733
SAabdullah31 <cse.asabdullah@gmail.com>
2020-03-21 13:00:12 +0600

    ema-jhon simple with react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect,} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>now i start react</p>
+        <h1>Done</h1>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
